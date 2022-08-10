82fd7dabbe205ee1b3544f77b1d7b0cc4d5f0719
fwcd <fwcdmail@gmail.com>
2022-08-10 04:30:35 +0200

    Add type definitions

---
+++ src/common/types.ts
@@ -0,0 +1,27 @@
+/** A verb used for a request. */
+export type Verb = 'POST' | 'CREATE' | 'MKDIR' | 'DELETE' | 'LIST' | 'GET' | 'PUT' | 'STREAM' | 'STOP' | 'LINK' | 'UNLINK';
+
+/** Authentication credentials. */
+export interface Auth {
+  USER: string;
+  TOKEN: string;
+}
+
+/** A message sent from the client. */
+export interface ClientMessage<P> {
+  REID: any;
+  AUTH: Auth;
+  VERB: Verb;
+  PATH: string[];
+  META: any;
+  PAYL: P;
+}
+
+/** A message sent from the server. */
+export interface ServerMessage<P> {
+  RNUM: number;
+  REID?: any;
+  RESPONSE?: string;
+  WARNINGS?: string[];
+  PAYL: P;
+}
