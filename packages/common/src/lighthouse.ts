e022a536f3219a0a943ad88115819fec6658fc19
fwcd <fwcdmail@gmail.com>
2022-08-10 16:21:10 +0200

    Handle responses

--- packages/common/src/lighthouse.ts
+++ packages/common/src/lighthouse.ts
@@ -6,6 +6,7 @@ import { Coder, MessagePackCoder } from "./coder";
 /** A connection to the lighthouse. */
 export class Lighthouse {
   private requestId: number = 0;
+  private responseHandlers: Map<number, (message: ServerMessage<unknown>) => void> = new Map();
 
   constructor(
     private readonly auth: Auth,
@@ -23,17 +24,17 @@ export class Lighthouse {
   // TODO: Input handling
 
   /** Sends a display. */
-  async sendDisplay(rgbValues: Uint8Array): Promise<void> {
-    this.sendRequest('PUT', ['user', this.auth.USER, 'model'], rgbValues);
+  async sendDisplay(rgbValues: Uint8Array): Promise<ServerMessage<unknown>> {
+    return this.sendRequest('PUT', ['user', this.auth.USER, 'model'], rgbValues);
   }
 
   /** Requests a stream. */
-  async requestStream(): Promise<void> {
-    this.sendRequest('STREAM', ['user', this.auth.USER, 'model'], {});
+  async requestStream(): Promise<ServerMessage<unknown>> {
+    return this.sendRequest('STREAM', ['user', this.auth.USER, 'model'], {});
   }
 
   /** Sends a request. */
-  async sendRequest<T>(verb: Verb, path: string[], payload: T): Promise<void> {
+  async sendRequest<T>(verb: Verb, path: string[], payload: T): Promise<ServerMessage<unknown>> {
     // TODO: Handle response
     const message: ClientMessage<T> = {
       AUTH: this.auth,
@@ -44,7 +45,9 @@ export class Lighthouse {
       PAYL: payload,
     };
     const raw = this.coder.encode(message);
+    const responsePromise = this.receiveResponse(message.REID);
     await this.transport.send(raw);
+    return await responsePromise;
   }
 
   /** Sends an arbitrary message. */
@@ -52,10 +55,32 @@ export class Lighthouse {
     const raw = this.coder.encode(message);
     this.transport.send(raw);
   }
+  
+  /** Asynchronously receives a response for the given request id. */
+  private async receiveResponse(id: number): Promise<ServerMessage<unknown>> {
+    return new Promise((resolve, reject) => {
+      this.responseHandlers[id] = message => {
+        if (message.RNUM === 200) {
+          resolve(message);
+        } else {
+          reject(JSON.stringify(message));
+        }
+      };
+    });
+  }
 
   /** Handles an arbitrary (received) message. */
   private async handle(message: ServerMessage<unknown>): Promise<void> {
-    // TODO
+    const id = message.REID;
+    if (id) {
+      const handler = this.responseHandlers.get(id);
+      if (handler) {
+        handler(message);
+        this.responseHandlers.delete(id);
+      }
+    } else {
+      this.logger.warning(`Unhandled message: ${JSON.stringify(message)}`);
+    }
   }
 
   async close(): Promise<void> {
