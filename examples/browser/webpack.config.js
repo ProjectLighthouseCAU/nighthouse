b56e79fa612224aed6dbaa89567bd649ae984924
fwcd <fwcdmail@gmail.com>
2022-08-10 03:53:34 +0200

    Regenerate project using webpack-cli init

--- examples/browser/webpack.config.js
+++ examples/browser/webpack.config.js
@@ -1,32 +1,59 @@
-const path = require('path');
-const CopyPlugin = require('copy-webpack-plugin');
+// Generated using webpack-cli https://github.com/webpack/webpack-cli
 
-const distDir = path.join(__dirname, 'dist');
-const publicDir = path.join(__dirname, 'public');
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
-module.exports = {
-  entry: './src/index.ts',
+const isProduction = process.env.NODE_ENV == "production";
+
+const stylesHandler = "style-loader";
+
+const config = {
+  entry: "./src/index.ts",
   output: {
-    filename: 'bundle.js',
-    path: distDir,
+    path: path.resolve(__dirname, "dist"),
   },
+  devServer: {
+    open: true,
+    host: "localhost",
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "index.html",
+    }),
+
+    // Add your plugins here
+    // Learn more about plugins from https://webpack.js.org/configuration/plugins/
+  ],
   module: {
     rules: [
       {
-        test: /.tsx?$/,
-        use: 'ts-loader',
-        exclude: /node_modules/
-      }
-    ]
+        test: /\.(ts|tsx)$/i,
+        loader: "ts-loader",
+        exclude: ["/node_modules/"],
+      },
+      {
+        test: /\.css$/i,
+        use: [stylesHandler, "css-loader"],
+      },
+      {
+        test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/i,
+        type: "asset",
+      },
+
+      // Add your rules for custom modules here
+      // Learn more about loaders from https://webpack.js.org/loaders/
+    ],
   },
   resolve: {
-    extensions: ['.ts', '.tsx', '.js']
+    extensions: [".tsx", ".ts", ".jsx", ".js", "..."],
   },
-  plugins: [
-    new CopyPlugin({
-      patterns: [
-        { from: publicDir, to: distDir }
-      ]
-    }),
-  ]
+};
+
+module.exports = () => {
+  if (isProduction) {
+    config.mode = "production";
+  } else {
+    config.mode = "development";
+  }
+  return config;
 };
