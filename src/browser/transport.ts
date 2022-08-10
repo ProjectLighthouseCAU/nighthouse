7989ac4f0734953f07536f7ede29b025bc36f560
fwcd <fwcdmail@gmail.com>
2022-08-10 02:38:25 +0200

    Update project structure

--- src/browser/transport.ts
+++ src/browser/transport.ts
@@ -1,3 +1,11 @@
 import { Transport } from "../common";
 
-export class BrowserTransport {}
+export class BrowserTransport implements Transport {
+  async send<T>(message: T): Promise<void> {
+    // TODO
+  }
+
+  onReceive<T>(handler: (message: T) => void): void {
+    
+  }
+}
