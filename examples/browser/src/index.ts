4bf6f734c2edf975fc2c8dbb0f6410566b653c99
fwcd <fwcdmail@gmail.com>
2022-08-10 04:24:23 +0200

    Establish WebSocket connection

--- examples/browser/src/index.ts
+++ examples/browser/src/index.ts
@@ -1,1 +1,13 @@
 import * as litehouse from "litehouse/browser";
+
+addEventListener('load', () => {
+  const urlField = document.getElementById('lighthouse-url') as HTMLInputElement;
+  const usernameField = document.getElementById('lighthouse-username') as HTMLInputElement;
+  const tokenField = document.getElementById('lighthouse-token') as HTMLInputElement;
+  const connectButton = document.getElementById('lighthouse-connect');
+
+  connectButton.addEventListener('click', async () => {
+    const lh = await litehouse.connect(urlField.value);
+    console.log('Connected!');
+  });
+});
